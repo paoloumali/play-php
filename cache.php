0b1686d8dd6fb00aa408a2933e68a2d6e2ff7caf
paoloumali <dev@paoloumali.com>
2020-06-18 10:18:53 +0800

    cache1

---
+++ cache.php
@@ -0,0 +1,12 @@
+<?php
+
+$current_day = date('j-l');
+
+$cached_day = isset($_COOKIE["c_last_visit_day"]) ? $_COOKIE["c_last_visit_day"] : null;
+
+if ($cached_day != $current_day) {
+    // set a cookie for 1 day
+    setcookie('c_last_visit_day', $current_day, time()+60*60*24, '/cache.php', 'play-php.paoloumali.com', true);
+}
+
+echo 'test cache';
